d49c0dfa8d5168e74d43cbd2bf2175af8e2ce1df
mohangaonkar <mohangaonkar.sky@gmail.com>
2024-10-10 17:29:10 +0530

    added videos and screenshots

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 @pytest.fixture(scope="session")
 def setup():
-    driver = webdriver.Chrome()  # or Firefox, depending on your setup
+    driver = webdriver.Edge()  # or Firefox, depending on your setup
     driver.maximize_window()
     driver.get("https://mysitebook.io/")  # login page
     yield driver
